87d7b3a831743214a88487cc49a9575f846c0c70
Santos-de-Israel <144865373+Santos-de-Israel@users.noreply.github.com>
2025-03-30 16:28:28 -0500

    footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ChatbotComponent } from '../../Components/chatbot/chatbot.component';
 import { Router } from '@angular/router';
+import { FooterComponent } from "../../Components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ChatbotComponent],
+  imports: [ChatbotComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
